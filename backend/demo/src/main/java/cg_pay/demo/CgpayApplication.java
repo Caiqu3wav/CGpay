ab3c0516662fc929ee857620cdf2cfa085bad953
Caique <asapcaique7@gmail.com>
2024-08-05 09:50:33 -0300

    definindo java no backend e nextjs no frontend

---
+++ backend/demo/src/main/java/cg_pay/demo/CgpayApplication.java
@@ -0,0 +1,13 @@
+package cg_pay.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgpayApplication.class, args);
+	}
+
+}
